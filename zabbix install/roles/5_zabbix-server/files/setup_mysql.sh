11962d1e08a644c9c04e1a74a5d67b9ad7587025
a365541453 <32005940+a365541453@users.noreply.github.com>
2017-12-02 16:49:16 +0800

    20171202

--- zabbix install/roles/5_zabbix-server/files/setup_mysql.sh
+++ zabbix install/roles/5_zabbix-server/files/setup_mysql.sh
@@ -1,22 +1,10 @@
 #! /bin/env bash
-
-
-mysql –u root –p
-
+mysql -u root -e'
 create database zabbix character set utf8;
-
-grant all privileges on zabbix.* to 'zabbix'@'localhost' identified by 'zabbix';
-
+use zabbix;
+grant all privileges on zabbix.* to "zabbix"@"%" IDENTIFIED BY "zabbix";
+use mysql;
+delete from user where user=" ";
 flush privileges;
-
-use zabbix
-
-source /usr/share/doc/zabbix-server-mysql-3.4.4/create.sql
-
-
-
-
-
-
-
-
+use zabbix;
+source /usr/share/doc/zabbix-server-mysql-3.4.4/create.sql'
